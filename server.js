b4d91679bf1223cbebe886c1a066d5895b003243
focus <iyaseeddyzin@gmail.com>
2026-07-04 13:59:53 +0100

    update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app  = express();
 const PORT = process.env.PORT || 3000;
 
 // ── DATABASE ──────────────────────────────────────────────────────────────────
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
